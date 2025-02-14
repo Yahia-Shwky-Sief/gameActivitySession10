af9510e9729bf4fd69d4612b63597a584287985d
YAHIA-SHWKY-SIEF <yahiasief4@gmail.com>
2025-02-14 20:04:38 +0200

    add init_game.js

---
+++ src/init_game.js
@@ -0,0 +1,5 @@
+export function init_game() {
+    setTimeout(() => {
+        console.log('Game initialized');
+    }, 1000);
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 //integrate all functions here
+import { init_game } from './init_game.js';
+init_game().then(() => {
+    
+});
